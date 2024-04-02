0cf7b3ddbd502769cd82c222d6cf7244a2c40f9d
Mikael Lund <Mlund@mac.com>
2024-04-02 15:59:06 +0200

    Add energy sum

--- anglescan/src/energy.rs
+++ anglescan/src/energy.rs
@@ -1,85 +1,53 @@
-use crate::structure::AtomKinds;
 use faunus::topology::AtomKind;
-use interact::multipole::{self, MultipoleEnergy, Yukawa};
-use interact::twobody::{IonIonYukawa, IsotropicTwobodyEnergy, LennardJones, YukawaLennardJones};
+use interact::multipole::Yukawa;
+use interact::twobody::{IonIon, IsotropicTwobodyEnergy, LennardJones, YukawaLennardJones};
 use interact::CombinationRule;
-use serde::de;
-use std::rc::Rc;
+
+use crate::structure::Structure;
 
 /// Pair-matrix of twobody energies for pairs of atom ids
-struct PairMatrix {
-    /// Matrix of twobody energies
+pub struct PairMatrix {
+    /// Matrix of twobody energy terms
     pub matrix: Vec<Vec<YukawaLennardJones>>,
-    pub multipole: Yukawa,
 }
 
 impl PairMatrix {
-    // fn build(&mut self, atomkinds: &[AtomKind]) {
-    //     let n = atomkinds.len();
-    //     // let multipole = Rc::new(Yukawa::new(0.0, Some(0.0)));
-    //     let default = YukawaLennardJones::new(
-    //         IonIonYukawa::new(0.0, &self.multipole),
-    //         LennardJones::new(0.0, 0.0),
-    //     );
-    //     self.matrix = vec![vec![default; n]; n];
-    //     for i in 0..n {
-    //         for j in 0..n {
-    //             let qq = atomkinds[i].charge * atomkinds[j].charge;
-    //             let epsilons = (
-    //                 atomkinds[i].epsilon.unwrap_or_default(),
-    //                 atomkinds[j].epsilon.unwrap_or_default(),
-    //             );
-    //             let sigmas = (
-    //                 atomkinds[i].sigma.unwrap_or_default(),
-    //                 atomkinds[j].sigma.unwrap_or_default(),
-    //             );
-    //             let lj = LennardJones::from_combination_rule(
-    //                 CombinationRule::LorentzBerthelot,
-    //                 epsilons,
-    //                 sigmas,
-    //             );
-    //             let ionion = IonIonYukawa::new(qq, &self.multipole);
-    //             self.matrix[i][j] = YukawaLennardJones::new(ionion, lj);
-    //         }
-    //     }
-    // }
-
     /// Create a new pair matrix
-    fn new(cutoff: f64, debye_length: f64, atomkinds: &[AtomKind]) -> Self {
-        // let multipole: &'static Yukawa = Yukawa::new(cutoff, Some(debye_length));
+    pub fn new(atomkinds: &[AtomKind], multipole: &'static Yukawa) -> Self {
+        let default =
+            YukawaLennardJones::new(IonIon::new(0.0, multipole), LennardJones::new(0.0, 0.0));
         let n = atomkinds.len();
-
-        let multipole = Yukawa::new(cutoff, Some(debye_length));
-        Self {
-            matrix: Vec::default(),
-            multipole: multipole.into(),
+        let mut matrix = vec![vec![default; n]; n];
+        for i in 0..n {
+            for j in 0..n {
+                let a = &atomkinds[i];
+                let b = &atomkinds[j];
+                let charge_product = a.charge * b.charge;
+                let epsilons = (a.epsilon.unwrap_or_default(), b.epsilon.unwrap_or_default());
+                let sigmas = (a.sigma.unwrap_or_default(), b.sigma.unwrap_or_default());
+                let ionion = IonIon::new(charge_product, multipole);
+                let lj = LennardJones::from_combination_rule(
+                    CombinationRule::LorentzBerthelot,
+                    epsilons,
+                    sigmas,
+                );
+                matrix[i][j] = YukawaLennardJones::new(ionion, lj);
+            }
         }
+        Self { matrix }
+    }
 
-        // let default = YukawaLennardJones::new(
-        //     IonIonYukawa::new(0.0, &result.multipole),
-        //     LennardJones::new(0.0, 0.0),
-        // );
-        // result.matrix = vec![vec![default; n]; n];
-        // for i in 0..n {
-        //     for j in 0..n {
-        //         let qq = atomkinds[i].charge * atomkinds[j].charge;
-        //         let epsilons = (
-        //             atomkinds[i].epsilon.unwrap_or_default(),
-        //             atomkinds[j].epsilon.unwrap_or_default(),
-        //         );
-        //         let sigmas = (
-        //             atomkinds[i].sigma.unwrap_or_default(),
-        //             atomkinds[j].sigma.unwrap_or_default(),
-        //         );
-        //         let lj = LennardJones::from_combination_rule(
-        //             CombinationRule::LorentzBerthelot,
-        //             epsilons,
-        //             sigmas,
-        //         );
-        //         let ionion = IonIonYukawa::new(qq, &result.multipole);
-        //         result.matrix[i][j] = YukawaLennardJones::new(ionion, lj);
-        //     }
-        // }
-        // result
+    // Sum energy between two set of atomic structures
+    pub fn sum_energy(&self, a: &Structure, b: &Structure) -> f64 {
+        let mut energy = 0.0;
+        for i in 0..a.positions.len() {
+            for j in 0..b.positions.len() {
+                let distance_sq = (a.positions[i] - b.positions[j]).norm_squared();
+                let id_a = a.atom_ids[i];
+                let id_b = b.atom_ids[j];
+                energy += self.matrix[id_a][id_b].isotropic_twobody_energy(distance_sq);
+            }
+        }
+        energy
     }
 }
