741652c89015646145ac5409cb239ae85720706b
Mikael Lund <mlund@mac.com>
2024-11-14 17:46:00 +0100

    Move particle to file

--- faunus/src/lib.rs
+++ faunus/src/lib.rs
@@ -16,7 +16,6 @@ use crate::group::{Group, GroupCollection};
 use cell::SimulationCell;
 use energy::Hamiltonian;
 use nalgebra::{DMatrix, Vector3};
-use serde::{Deserialize, Serialize};
 use std::{
     cell::{Ref, RefMut},
     rc::Rc,
@@ -51,58 +50,9 @@ pub use physical_constants::{
     VACUUM_ELECTRIC_PERMITTIVITY,
 };
 
-#[allow(dead_code)]
-trait PointParticle {
-    /// Type of the particle identifier
-    type Idtype;
-    /// Type of the particle position
-    type Positiontype;
-    /// Identifier for the particle type
-    fn atom_id(&self) -> Self::Idtype;
-    /// Get position
-    fn pos(&self) -> &Self::Positiontype;
-    /// Get mutable position
-    fn pos_mut(&mut self) -> &mut Self::Positiontype;
-    /// Index in main list of particle (immutable)
-    fn index(&self) -> usize;
-}
+mod particle;
+pub use particle::{Particle, PointParticle};
 
-#[derive(Serialize, Deserialize, Default, Debug, Clone, PartialEq)]
-pub struct Particle {
-    /// Type of the particle (index of the atom kind)
-    atom_id: usize,
-    /// Index in main list of particles
-    index: usize,
-    /// Position of the particle
-    pos: Point,
-}
-
-impl Particle {
-    pub(crate) fn new(atom_id: usize, index: usize, pos: Point) -> Particle {
-        Particle {
-            atom_id,
-            index,
-            pos,
-        }
-    }
-}
-
-impl PointParticle for Particle {
-    type Idtype = usize;
-    type Positiontype = Point;
-    fn atom_id(&self) -> Self::Idtype {
-        self.atom_id
-    }
-    fn pos(&self) -> &Self::Positiontype {
-        &self.pos
-    }
-    fn pos_mut(&mut self) -> &mut Self::Positiontype {
-        &mut self.pos
-    }
-    fn index(&self) -> usize {
-        self.index
-    }
-}
 pub trait SyncFrom {
     /// Synchronize internal state from another object of the same type
     fn sync_from(&mut self, other: &Self, change: &Change) -> anyhow::Result<()>;
