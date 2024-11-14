1c3770dbf3d64df16f5e3adc21d0016a1174902c
Mikael Lund <mlund@mac.com>
2024-11-14 17:07:16 +0100

    Split debye_length to separate file

--- coulomb/src/lib.rs
+++ coulomb/src/lib.rs
@@ -91,8 +91,8 @@ pub type Vector3 = nalgebra::Vector3<f64>;
 /// A 3x3 matrix
 pub type Matrix3 = nalgebra::Matrix3<f64>;
 
-const ANGSTROM_PER_METER: f64 = 1e10;
-const LITER_PER_ANGSTROM3: f64 = 1e-27;
+pub(crate) const ANGSTROM_PER_METER: f64 = 1e10;
+pub(crate) const LITER_PER_ANGSTROM3: f64 = 1e-27;
 
 mod cutoff;
 pub use cutoff::Cutoff;
@@ -107,91 +107,15 @@ pub use medium::Medium;
 pub use salt::Salt;
 mod temperature;
 pub use temperature::Temperature;
+mod ionic_strength;
+pub use ionic_strength::IonicStrength;
+mod debye_length;
+pub use debye_length::{bjerrum_length, debye_length, DebyeLength};
 
-use anyhow::Result;
 use physical_constants::{
-    AVOGADRO_CONSTANT, BOLTZMANN_CONSTANT, ELEMENTARY_CHARGE, MOLAR_GAS_CONSTANT,
-    VACUUM_ELECTRIC_PERMITTIVITY,
+    AVOGADRO_CONSTANT, ELEMENTARY_CHARGE, MOLAR_GAS_CONSTANT, VACUUM_ELECTRIC_PERMITTIVITY,
 };
 use std::f64::consts::PI;
-
-/// Trait for objects that has an ionic strength
-pub trait IonicStrength {
-    /// Get the ionic strength in mol/l
-    ///
-    /// The default implementation returns `None`.
-    fn ionic_strength(&self) -> Option<f64> {
-        None
-    }
-    /// Try to set the ionic strength in mol/l
-    ///
-    /// The default implementation returns an error.
-    fn set_ionic_strength(&mut self, _ionic_strength: f64) -> Result<()> {
-        Err(anyhow::anyhow!(
-            "Setting the ionic strength is not implemented"
-        ))
-    }
-}
-
-/// Trait for objects where a Debye length can be calculated
-pub trait DebyeLength: IonicStrength + permittivity::RelativePermittivity + Temperature {
-    /// # Debye length in angstrom or `None` if the ionic strength is zero.
-    ///
-    /// May perform expensive operations so avoid use in speed critical code,
-    /// such as inside tight interaction loops.
-    fn debye_length(&self) -> Option<f64> {
-        let temperature = self.temperature();
-        let permittivity = self.permittivity(temperature).unwrap();
-        self.ionic_strength()
-            .map(|i| debye_length(temperature, permittivity, i))
-    }
-    /// Inverse Debye length in inverse angstrom or `None` if the ionic strength is zero.
-    ///
-    /// May perform expensive operations so avoid use in speed critical code,
-    /// such as inside tight interaction loops.
-    fn kappa(&self) -> Option<f64> {
-        self.debye_length().map(f64::recip)
-    }
-}
-
-/// Calculates the Bjerrum length, λ𝐵 = e²/4πε𝑘𝑇 commonly used in electrostatics (ångström).
-///
-/// More information at <https://en.wikipedia.org/wiki/Bjerrum_length>.
-///
-/// # Examples
-/// ~~~
-/// use coulomb::bjerrum_length;
-/// let lB = bjerrum_length(293.0, 80.0); // angstroms
-/// assert_eq!(lB, 7.1288799871283);
-/// ~~~
-pub fn bjerrum_length(kelvin: f64, relative_permittivity: f64) -> f64 {
-    ELEMENTARY_CHARGE.powi(2) * ANGSTROM_PER_METER
-        / (4.0
-            * PI
-            * relative_permittivity
-            * VACUUM_ELECTRIC_PERMITTIVITY
-            * BOLTZMANN_CONSTANT
-            * kelvin)
-}
-
-/// Calculates the Debye length in angstrom, λ𝐷 = 1/√(8π·λ𝐵·𝐼·𝑁𝐴·𝑉), where 𝐼 is the ionic strength in molar units (mol/l).
-///
-/// # Examples
-/// ~~~
-/// use coulomb::debye_length;
-/// let molarity = 0.03;                              // mol/l
-/// let lambda = debye_length(293.0, 80.0, molarity); // angstroms
-/// assert_eq!(lambda, 17.576538097378368);
-/// ~~~
-pub fn debye_length(kelvin: f64, relative_permittivity: f64, ionic_strength: f64) -> f64 {
-    (8.0 * PI
-        * bjerrum_length(kelvin, relative_permittivity)
-        * ionic_strength
-        * AVOGADRO_CONSTANT
-        * LITER_PER_ANGSTROM3)
-        .sqrt()
-        .recip()
-}
 
 /// Electrostatic prefactor, e²/4πε₀ × 10⁷ × NA [Å × kJ / mol].
 ///
